7aa1fcc1adab5353ac45e23c6be4f8300b92c2c3
dujinjin <dujinjin@duiba.com.cn>
2019-06-14 15:47:11 +0800

    骨架子搭好

--- src/router.js
+++ src/router.js
@@ -1,26 +1,118 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Nprogress from "nprogress";
+import "nprogress/nprogress.css";
+import NotFound from "./views/404";
 
 Vue.use(Router);
-
-export default new Router({
+console.log("process.env.BASE_URL", process.env.BASE_URL);
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: "/user",
+      component: () =>
+        import(/* webpackChunkName: "userlayout" */ "./layouts/UserLayout.vue"),
+      children: [
+        {
+          path: "/user",
+          redirect: "/user/login"
+        },
+        {
+          path: "/user/login",
+          name: "login",
+          component: () =>
+            import(/* webpackChunkName: "user" */ "./views/User/Login.vue")
+        },
+        {
+          path: "/user/register",
+          name: "register",
+          component: () =>
+            import(/* webpackChunkName: "register" */ "./views/User/Register.vue")
+        }
+      ]
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: "/",
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "layout" */ "./layouts/BasicLayout"),
+      children: [
+        {
+          path: "/",
+          redirect: "/dashboard/analysis"
+        },
+        {
+          path: "/dashboard",
+          name: "dashboard",
+          component: { render: h => h("router-view") },
+          children: [
+            {
+              path: "/dashboard/analysis",
+              name: "analysis",
+              component: () =>
+                import(/* webpackChunkName: "dashboard" */ "./views/Dashboard/Analysis")
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: "/form",
+      name: "form",
+      component: { render: h => h("router-view") },
+      children: [
+        {
+          path: "/form/basic-form",
+          name: "basicform",
+          component: () =>
+            import(/* webpackChunkName: "form" */ "./views/Forms/BasicForm")
+        },
+        {
+          path: "/form/step-form",
+          name: "stepform",
+          component: () =>
+            import(/* webpackChunkName: "form" */ "./views/Forms/StepForm"),
+          children: [
+            {
+              path: "/form/step-form",
+              redirect: "/form/step-form/info"
+            },
+            {
+              path: "/form/step-form/info",
+              name: "info",
+              component: () =>
+                import(/* webpackChunkName: "form" */ "./views/Forms/StepForm/Step1")
+            },
+            {
+              path: "/form/step-form/confirm",
+              name: "confirm",
+              component: () =>
+                import(/* webpackChunkName: "form" */ "./views/Forms/StepForm/Step2")
+            },
+            {
+              path: "/form/step-form/result",
+              name: "result",
+              component: () =>
+                import(/* webpackChunkName: "form" */ "./views/Forms/StepForm/Step3")
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: "*",
+      name: "404",
+      component: NotFound
     }
   ]
 });
+router.beforeEach((to, from, next) => { /* eslint-disable-line */
+  Nprogress.start();
+  next();
+});
+router.afterEach((to, from) => { /* eslint-disable-line */
+  Nprogress.done();
+});
+
+export default router;
