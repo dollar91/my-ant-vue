e56812e5a624e46db43a4e624472a3b5fb18c5ce
dujinjin <dujinjin@duiba.com.cn>
2019-06-19 14:12:17 +0800

    分布表单

---
+++ src/store/modules/form.js
@@ -0,0 +1,31 @@
+import router from "../../router";
+import request from "../../utils/request";
+export default {
+  namespaced: true,
+  state: {
+    step: {
+      payAccount: "123456",
+      password: "111"
+    }
+  },
+  actions: {
+    async submitStepForm({ commit }, { payload }) {
+      await request({
+        url: "/api/form",
+        method: "POST",
+        data: payload
+      });
+      commit("saveStepFormData", { payload });
+      router.push("/form/step-form/result");
+    }
+  },
+  mutations: {
+    saveStepFormData(state, { payload }) {
+      state.step = {
+        ...state.step,
+        ...payload
+      };
+    }
+  },
+  getters: {}
+};
